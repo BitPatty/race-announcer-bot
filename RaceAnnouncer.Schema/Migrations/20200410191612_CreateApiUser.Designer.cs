8adcb899976781cfb342809b349b5a7d015a5a97
Matteias Collet <matteias.collet@bluewin.ch>
2020-04-10 21:24:58 +0200

    add ApiUser model

--- RaceAnnouncer.Schema/Migrations/20200410191612_CreateApiUser.Designer.cs
+++ RaceAnnouncer.Schema/Migrations/20200410191612_CreateApiUser.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RaceAnnouncer.Schema;
 
 namespace RaceAnnouncer.Schema.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20200410191612_CreateApiUser")]
+    partial class CreateApiUser
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ RaceAnnouncer.Schema/Models/Configurations/APIUserConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using RaceAnnouncer.Schema.Models.BaseModels.Configurations;
+
+namespace RaceAnnouncer.Schema.Models.Configurations
+{
+  public class APIUserConfiguration : BaseEntityConfiguration<APIUser>
+  {
+    public override void Configure(EntityTypeBuilder<APIUser> builder)
+    {
+      builder.HasIndex(p => p.Username).IsUnique();
+
+      base.Configure(builder);
+    }
+  }
+}
