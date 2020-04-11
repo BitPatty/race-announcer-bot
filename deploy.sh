514248c032ad03d6d415d0475c3be7260a860b9e
Matteias Collet <matteias.collet@bluewin.ch>
2020-04-11 02:10:22 +0200

    add docker-compose

--- deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-docker stop raceannouncerbot
-docker container rm raceannouncerbot
-docker image rm raceannouncer
-
-docker build -f RaceAnnouncer.Bot/Dockerfile -t raceannouncer .
-docker run -d --name raceannouncerbot --restart always raceannouncer
