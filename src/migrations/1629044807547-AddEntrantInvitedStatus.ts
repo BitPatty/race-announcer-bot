11c123323e59faf15734dd9b417df8524c8fd86b
Matteias Collet <matteias.collet@bluewin.ch>
2021-08-15 16:32:20 +0000

    remove database name from migrations

--- src/migrations/1629044807547-AddEntrantInvitedStatus.ts
+++ src/migrations/1629044807547-AddEntrantInvitedStatus.ts
@@ -9,13 +9,13 @@ export class AddEntrantInvitedStatus1629044807547
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `ALTER TABLE \`development\`.\`entrant\` CHANGE \`status\` \`status\` enum ('unknown', 'entered', 'ready', 'forfeit', 'done', 'disqualified', 'invited') NOT NULL`,
+      `ALTER TABLE \`entrant\` CHANGE \`status\` \`status\` enum ('unknown', 'entered', 'ready', 'forfeit', 'done', 'disqualified', 'invited') NOT NULL`,
     );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `ALTER TABLE \`development\`.\`entrant\` CHANGE \`status\` \`status\` enum ('unknown', 'entered', 'ready', 'forfeit', 'done', 'disqualified') NOT NULL`,
+      `ALTER TABLE \`entrant\` CHANGE \`status\` \`status\` enum ('unknown', 'entered', 'ready', 'forfeit', 'done', 'disqualified') NOT NULL`,
     );
   }
 }
