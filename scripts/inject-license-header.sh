17bae94ec2bc84620c4cee1487f43f0b04d5268f
Matteias Collet <matteias.collet@bluewin.ch>
2021-10-04 01:41:03 +0000

    move license header to script folder

--- scripts/inject-license-header.sh
+++ scripts/inject-license-header.sh
@@ -6,6 +6,6 @@ for i in $(find $DIRNAME/../src -name '*.ts')
 do
   if ! grep -q Copyright $i
   then
-    cat LICENSE_HEADER $i >$i.new && mv $i.new $i
+    cat ./scripts/LICENSE_HEADER $i >$i.new && mv $i.new $i
   fi
 done
